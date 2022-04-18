9f2793a2cf59bcb94034b918b7c03cebcc3a0ab4
Rajesh1243 <potrajes123@gmail.com>
2022-04-18 13:49:42 +0530

    g

--- com/main/java/com/cts/controller/Application.java
+++ com/main/java/com/cts/controller/Application.java
@@ -13,7 +13,7 @@ import org.springframework.web.client.RestTemplate;
 @ComponentScan(basePackages = { "com.cts" })
 @SpringBootApplication
 public class Application {
-//g
+//ggg
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 
